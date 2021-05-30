59f6605dac156156ccd66a81efc3b6bcfa69e5ad
MasiBall <76224425+MasiBall@users.noreply.github.com>
2021-05-30 15:55:08 +0300

    Accident happened

--- mball_log/client/cl_log.lua
+++
@@ -1,9 +0,0 @@
-local playtime = 0
-
-Citizen.CreateThread(function()
-    while true do
-        playtime = playtime + 1
-        TriggerServerEvent('log:server:playtime', playtime)
-        Citizen.Wait(60000)
-    end
-end)
