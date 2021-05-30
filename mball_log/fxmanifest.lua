b3b19382f485623c257cdf3d42ff4560a85bae22
MasiBall <76224425+MasiBall@users.noreply.github.com>
2021-05-30 15:52:22 +0300

    Added playtime counter and try to fix timestamp

---
+++ mball_log/fxmanifest.lua
@@ -0,0 +1,13 @@
+fx_version 'cerulean'
+games { 'gta5' }
+
+author 'MasiBall'
+Description 'Basic Standalone Join/Leave logs with identifiers'
+
+client_scripts {
+	'client/cl_log.lua'
+}
+
+server_scripts {
+	'server/sv_log.lua'
+}
