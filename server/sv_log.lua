93d27806dd8aa40b71bf0ad6f884ce76e28f6de2
MasiBall <76224425+MasiBall@users.noreply.github.com>
2021-06-20 21:26:59 +0300

    Code cleanup

--- server/sv_log.lua
+++ server/sv_log.lua
@@ -14,46 +14,8 @@ end)
 AddEventHandler('playerConnecting', function()
   local _source = source
   local name = GetPlayerName(_source)
-  local license = nil
-  local identifier = nil
-	local discord = nil
-  local xbl = nil
-  local live = nil
-  local fivem = nil
-	for k,v in ipairs(GetPlayerIdentifiers(_source))do
-		if string.sub(v, 1, string.len("license:")) == "license:" then
-		  license = v
-		elseif string.sub(v, 1, string.len("steam:")) == "steam:" then
-			identifier = v
-		elseif string.sub(v, 1, string.len("discord:")) == "discord:" then
-			discord = v
-    elseif string.sub(v, 1, string.len("xbl:")) == "xbl:" then
-      xbl = v
-    elseif string.sub(v, 1, string.len("live:")) == "live:" then
-      live = v
-    elseif string.sub(v, 1, string.len("fivem:")) == "fivem:" then
-      fivem = v
-	  end
-  end
-  if license == nil then
-    license = "No License found"
-  end
-  if identifier == nil then
-    identifier = "No Hex-ID found"
-  end
-  if discord == nil then
-    discord = "No Discord found"
-  end
-  if xbl == nil then
-    xbl = "No xbl ID found"
-  end
-  if live == nil then
-    live = "No Live ID found"
-  end
-  if fivem == nil then
-    fivem = "No FiveM ID found"
-  end
-    sendToDiscordLogsEmbed(3158326, '`✅` | PLAYER CONNECTING',' Player: `' .. name .. '`\n Hex-ID: `' ..identifier.. '`\n License: `' ..license.. '`\n Discord ID: `' ..discord.. '`\n XBL ID: `' ..xbl.. '`\n Live ID: `' ..live.. '`\n FiveM ID: `' ..fivem.. '`\n IP: `' ..GetPlayerEndpoint(_source).. '`')
+  local mb = Masipallopaa(_source)
+    sendToDiscordLogsEmbed(3158326, '`✅` | PLAYER CONNECTING',' Player: `' .. name .. '`\n Hex-ID: `' ..mb.identifier.. '`\n License: `' ..mb.license.. '`\n Discord ID: `' ..mb.discord.. '`\n XBL ID: `' ..mb.xbl.. '`\n Live ID: `' ..mb.live.. '`\n FiveM ID: `' ..mb.fivem.. '`\n IP: `' ..GetPlayerEndpoint(_source).. '`')
 end)
 
 --Player Leaving
@@ -61,46 +23,8 @@ AddEventHandler('playerDropped', function(reason)
   local _source = source
   local name = GetPlayerName(_source)
   local playtime = playertimes[_source] or 0
-  local license = nil
-  local identifier = nil
-	local discord = nil
-  local xbl = nil
-  local live = nil
-  local fivem = nil
-  for k,v in ipairs(GetPlayerIdentifiers(_source))do
-    if string.sub(v, 1, string.len("license:")) == "license:" then
-      license = v
-    elseif string.sub(v, 1, string.len("steam:")) == "steam:" then
-      identifier = v
-    elseif string.sub(v, 1, string.len("discord:")) == "discord:" then
-      discord = v
-    elseif string.sub(v, 1, string.len("xbl:")) == "xbl:" then
-      xbl = v
-    elseif string.sub(v, 1, string.len("live:")) == "live:" then
-      live = v
-    elseif string.sub(v, 1, string.len("fivem:")) == "fivem:" then
-      fivem = v
-    end
-  end
-  if license == nil then
-    license = "No License found"
-  end
-  if identifier == nil then
-    identifier = "No Hex-ID found"
-  end
-  if discord == nil then
-    discord = "No Discord found"
-  end
-  if xbl == nil then
-    xbl = "No xbl ID found"
-  end
-  if live == nil then
-    live = "No Live ID found"
-  end
-  if fivem == nil then
-    fivem = "No FiveM ID found"
-  end
-    sendToDiscordLogsEmbed(3158326, '`❌` | PLAYER DROPPED',' Reason: `' ..reason.. '`\n Player: `' ..name.. '`\n Playtime: `' ..playtime.. '` Minutes \n Hex-ID: `' ..identifier.. '`\n License: `' ..license.. '`\n Discord ID: `' ..discord.. '`\n XBL ID: `' ..xbl.. '`\n Live ID: `' ..live.. '`\n FiveM ID: `' ..fivem.. '`\n IP: `' ..GetPlayerEndpoint(_source).. '`')
+  local mb = Masipallopaa(_source)
+    sendToDiscordLogsEmbed(3158326, '`❌` | PLAYER DROPPED',' Reason: `' ..reason.. '`\n Player: `' ..name.. '`\n Playtime: `' ..playtime.. '` Minutes \n Hex-ID: `' ..mb.identifier.. '`\n License: `' ..mb.license.. '`\n Discord ID: `' ..mb.discord.. '`\n XBL ID: `' ..mb.xbl.. '`\n Live ID: `' ..mb.live.. '`\n FiveM ID: `' ..mb.fivem.. '`\n IP: `' ..GetPlayerEndpoint(_source).. '`')
 end)
 
 function sendToDiscordLogsEmbed(color, name, message, footer)
@@ -117,4 +41,32 @@ function sendToDiscordLogsEmbed(color, name, message, footer)
     }
 
   PerformHttpRequest(discordwebhook, function(err, text, headers) end, 'POST', json.encode({username = 'Logs', embeds = embed}), { ['Content-Type'] = 'application/json' })
+end
+
+function Masipallopaa(_source)
+  local idtablemb = {
+    license = "No License found",
+    identifier = "No Hex-ID found",
+    discord = "No Discord found",
+    xbl = "No xbl ID found",
+    live = "No Live ID found",
+    fivem = "No FiveM ID found"
+  }
+
+    for k,v in ipairs(GetPlayerIdentifiers(_source))do
+    if string.sub(v, 1, string.len("license:")) == "license:" then
+      idtablemb.license = v
+    elseif string.sub(v, 1, string.len("steam:")) == "steam:" then
+      idtablemb.identifier = v
+    elseif string.sub(v, 1, string.len("discord:")) == "discord:" then
+      idtablemb.discord = v
+    elseif string.sub(v, 1, string.len("xbl:")) == "xbl:" then
+      idtablemb.xbl = v
+    elseif string.sub(v, 1, string.len("live:")) == "live:" then
+      idtablemb.live = v
+    elseif string.sub(v, 1, string.len("fivem:")) == "fivem:" then
+      idtablemb.fivem = v
+    end
+  end
+  return idtablemb
 end
